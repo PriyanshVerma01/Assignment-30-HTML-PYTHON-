6776988a41aa676300c83eb31254c68663fac2f9
PRIYANSH01 <priyanshverma9@gmail.com>
2022-11-06 14:55:59 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Assignment: 30 HTML
+## Python Web Development in Hindi
+### MySirG
---
+++ swap.py
@@ -0,0 +1,5 @@
+a = 10
+b = 40
+print("\nBefore swap a = %d and b = %d" %(a, b))
+print("\nAfter swaping a = %d and b = %d" %(a, b))
+print()
